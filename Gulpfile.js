e3ba9773e07d337b1d3492d08f0849ff68f5559a
OnlyByDesign <benyannella@gmail.com>
2020-05-17 04:31:23 -0400

    updated SASS files | added CSS and JS modules | starting to add layout chunks | set variables | set kindling | importing fonts

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,33 +1,35 @@
 const gulp = require('gulp');
-// const nunjucks = require('gulp-nunjucks');
 const nunjucksRender = require('gulp-nunjucks-render');
 const data = require('gulp-data');
-const pages = [
-    "index","experience"
-]
 
+
+const pages = [
+    "index",
+    "experience"
+]
 gulp.task('nunjucks', async function() {
     return pages.forEach(function(current) {
-        gulp.src('App/pages/'+current+'.+(html|njk)')
+        gulp.src(`App/pages/${current}.+(html|njk)`)
             .pipe(data(function() {
-                return require('./App/content_'+current+'.json')
+                return require(`./App/content_${current}.json`)
             }))
             .pipe(nunjucksRender({
                 path: ['App/templates']
             }))
             .pipe(gulp.dest('App'))
     })
+});
+gulp.task('css', function () {
+    const postcss = require('gulp-postcss')
+  
+    return gulp.src('App/css/output.css')
+      .pipe(postcss([
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ]))
+      .pipe(gulp.dest('App/css'))
 })
-/*gulp.task('nunjucks', function() {
-    return gulp.src('App/pages/*.+(html|njk)')
-        .pipe(data(function() {
-            return require('./App/content.json')
-        }))
-        .pipe(nunjucksRender({
-            path: ['App/templates']
-        }))
-        .pipe(gulp.dest('App'))
-})*/
+
 
 exports.default = function() {
     //
