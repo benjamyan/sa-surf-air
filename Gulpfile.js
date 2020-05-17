2d76da04f582d3cb2dcd84b9e9a61124523afdf9
OnlyByDesign <benyannella@gmail.com>
2020-05-17 03:03:32 -0400

    Added basic JS files for interactions | updated gulp process to accomodate multiple JSON files | added page title macro

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,41 +1,34 @@
 const gulp = require('gulp');
-const nunjucks = require('gulp-nunjucks');
+// const nunjucks = require('gulp-nunjucks');
 const nunjucksRender = require('gulp-nunjucks-render');
 const data = require('gulp-data');
+const pages = [
+    "index","experience"
+]
 
-gulp.task('nunjucks', function() {
-    // Gets .html and .njk files in pages
-    return gulp.src('App/pages/*.+(html|njk)')
-    // import the data
-    .pipe(data(function() {
-        return require('./App/content.json')
-    }))
-    // Renders template with nunjucks
-    .pipe(nunjucksRender({
-        path: ['App/templates']
-    }))
-    // output files in app folder
-    .pipe(gulp.dest('App'))
+gulp.task('nunjucks', async function() {
+    return pages.forEach(function(current) {
+        gulp.src('App/pages/'+current+'.+(html|njk)')
+            .pipe(data(function() {
+                return require('./App/content_'+current+'.json')
+            }))
+            .pipe(nunjucksRender({
+                path: ['App/templates']
+            }))
+            .pipe(gulp.dest('App'))
+    })
 })
-
-function clean(cb) {
-    // body omitted
-    cb();
-}
-
-function javascript(cb) {
-    // body omitted
-    cb();
-}
-
-function css(cb) {
-    // body omitted
-    cb();
-}
+/*gulp.task('nunjucks', function() {
+    return gulp.src('App/pages/*.+(html|njk)')
+        .pipe(data(function() {
+            return require('./App/content.json')
+        }))
+        .pipe(nunjucksRender({
+            path: ['App/templates']
+        }))
+        .pipe(gulp.dest('App'))
+})*/
 
 exports.default = function() {
-    // You can use a single task
-    watch('src/*.css', css);
-    // Or a composed task
-    watch('src/*.js', series(clean, javascript));
+    //
 };
